ca69bb6ada2a25b2d4805900f69dd47638023672
prernaraghuwanshi <raghuwanshi.prerna@gmail.com>
2020-10-25 00:29:40 +0530

    [Prerna]REFACTORED: Ride class to incorporate service type

--- src/main/java/com/bl/InvoiceService/Ride.java
+++ src/main/java/com/bl/InvoiceService/Ride.java
@@ -3,25 +3,29 @@ package com.bl.InvoiceService;
 public class Ride {
     private double distance;
     private int time;
+    private String type;
 
     public Ride(double distance, int time) {
         this.distance = distance;
         this.time = time;
+        this.type = "normal";
+    }
+
+    public Ride(double distance, int time, String type) {
+        this.distance = distance;
+        this.time = time;
+        this.type = type;
     }
 
     public double getDistance() {
         return distance;
     }
 
-    public void setDistance(double distance) {
-        this.distance = distance;
-    }
-
     public int getTime() {
         return time;
     }
 
-    public void setTime(int time) {
-        this.time = time;
+    public String getType() {
+        return type;
     }
 }
