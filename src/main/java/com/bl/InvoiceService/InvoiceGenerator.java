0b81b434bb8fc1da963657172eff75e6f27dd916
prernaraghuwanshi <raghuwanshi.prerna@gmail.com>
2020-10-25 00:30:19 +0530

    [Prerna]ADDED: Function to generate invoice summary for premium rider

--- src/main/java/com/bl/InvoiceService/InvoiceGenerator.java
+++ src/main/java/com/bl/InvoiceService/InvoiceGenerator.java
@@ -5,17 +5,29 @@ import com.bl.Exception.InvoiceSummaryException;
 import java.util.Arrays;
 
 public class InvoiceGenerator {
-
-    private static final double MINIMUM_COST_PER_KILOMETER = 10;
-    private static final double MINIMUM_COST_PER_MINUTE = 1;
-    private static final double MINIMUM_FARE = 5;
+    private static double MINIMUM_COST_PER_KILOMETER;
+    private static double MINIMUM_COST_PER_MINUTE;
+    private static double MINIMUM_FARE;
     private RideRepository rideRepository = new RideRepository();
 
     public void welcomeMessage() {
         System.out.println("Welcome to Cab Invoice Generator!");
     }
 
-    public double calculateFare(double distance, int time) {
+    private void setInvoiceConstants(String rideType) {
+        if (rideType.equalsIgnoreCase("normal")) {
+            MINIMUM_COST_PER_KILOMETER = 10;
+            MINIMUM_COST_PER_MINUTE = 1;
+            MINIMUM_FARE = 5;
+        } else if (rideType.equalsIgnoreCase("premium")) {
+            MINIMUM_COST_PER_KILOMETER = 15;
+            MINIMUM_COST_PER_MINUTE = 2;
+            MINIMUM_FARE = 20;
+        }
+    }
+
+    public double calculateFare(double distance, int time, String rideType) {
+        setInvoiceConstants(rideType);
         double totalFare = MINIMUM_COST_PER_KILOMETER * distance + MINIMUM_COST_PER_MINUTE * time;
         if (totalFare < MINIMUM_FARE)
             totalFare = MINIMUM_FARE;
@@ -23,7 +35,7 @@ public class InvoiceGenerator {
     }
 
     public double calculateFare(Ride[] rides) {
-        double totalFare = Arrays.stream(rides).mapToDouble(ride -> calculateFare(ride.getDistance(), ride.getTime())).sum();
+        double totalFare = Arrays.stream(rides).mapToDouble(ride -> calculateFare(ride.getDistance(), ride.getTime(), ride.getType())).sum();
         return totalFare;
     }
 
