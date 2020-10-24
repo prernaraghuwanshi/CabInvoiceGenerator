c6347cd7a6a8813c48ca6eb1ee8ee6d8cbba440d
prernaraghuwanshi <raghuwanshi.prerna@gmail.com>
2020-10-24 22:23:12 +0530

    [Prerna]ADDED: Function to calculate total fare given distance and time

--- src/main/java/com/bl/InvoiceService/InvoiceGenerator.java
+++ src/main/java/com/bl/InvoiceService/InvoiceGenerator.java
@@ -2,8 +2,15 @@ package com.bl.InvoiceService;
 
 public class InvoiceGenerator {
 
+    private static final double MINIMUM_COST_PER_KILOMETER = 10;
+    private static final double MINIMUM_COST_PER_MINUTE = 1;
+
     public void welcomeMessage() {
         System.out.println("Welcome to Cab Invoice Generator!");
     }
 
+    public double calculateFare(double distance, int time) {
+        double fare = MINIMUM_COST_PER_KILOMETER * distance + MINIMUM_COST_PER_MINUTE * time;
+        return fare;
+    }
 }
