b0c6cffa7847d25174da6098072361ca29537314
prernaraghuwanshi <raghuwanshi.prerna@gmail.com>
2020-10-24 22:22:29 +0530

    [Prerna]ADDED: Test case to calculate total fare given distance and time

--- src/test/java/com/bl/InvoiceService/InvoiceServiceTest.java
+++ src/test/java/com/bl/InvoiceService/InvoiceServiceTest.java
@@ -12,4 +12,14 @@ public class InvoiceServiceTest {
     public void initialize() {
         invoiceGenerator = new InvoiceGenerator();
     }
+
+    @Test
+    public void givenDistanceAndTime_shouldReturnTotalFare()
+    {
+        double distance = 2.0;
+        int time = 5;
+        double totalFare = invoiceGenerator.calculateFare(distance, time);
+        Assert.assertEquals(25,totalFare,0.0);
+    }
+
 }
