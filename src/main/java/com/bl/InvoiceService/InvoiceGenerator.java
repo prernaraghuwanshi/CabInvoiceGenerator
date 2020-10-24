133a6f647d7a2b17d11139271a9ad76ade4df10b
prernaraghuwanshi <raghuwanshi.prerna@gmail.com>
2020-10-24 22:29:57 +0530

    [Prerna]REFACTORED: calculateFare function to accomodate minimum fare criteria

--- src/main/java/com/bl/InvoiceService/InvoiceGenerator.java
+++ src/main/java/com/bl/InvoiceService/InvoiceGenerator.java
@@ -4,6 +4,7 @@ public class InvoiceGenerator {
 
     private static final double MINIMUM_COST_PER_KILOMETER = 10;
     private static final double MINIMUM_COST_PER_MINUTE = 1;
+    private static final double MINIMUM_FARE = 5;
 
     public void welcomeMessage() {
         System.out.println("Welcome to Cab Invoice Generator!");
@@ -11,6 +12,8 @@ public class InvoiceGenerator {
 
     public double calculateFare(double distance, int time) {
         double fare = MINIMUM_COST_PER_KILOMETER * distance + MINIMUM_COST_PER_MINUTE * time;
+        if (fare < MINIMUM_FARE)
+            fare = MINIMUM_FARE;
         return fare;
     }
 }
