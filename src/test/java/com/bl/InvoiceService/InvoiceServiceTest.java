96dbd52001224de2ea4d67480db17af993a7f28b
prernaraghuwanshi <raghuwanshi.prerna@gmail.com>
2020-10-24 22:36:46 +0530

    [Prerna]ADDED: Test case for total fare of multiple rides

--- src/test/java/com/bl/InvoiceService/InvoiceServiceTest.java
+++ src/test/java/com/bl/InvoiceService/InvoiceServiceTest.java
@@ -29,4 +29,13 @@ public class InvoiceServiceTest {
         Assert.assertEquals(5, totalFare, 0.0);
     }
 
+    @Test
+    public void givenMultipleRides_shouldReturnTotalFare() {
+        Ride[] rides = {
+                new Ride(2.0, 5),
+                new Ride(0.1, 1)
+        };
+        double totalFare = invoiceGenerator.calculateFare(rides);
+        Assert.assertEquals(30, totalFare, 0.0);
+    }
 }
