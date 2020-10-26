e11eb063ee41bece410ce61d5734eccfeb143af9
prernaraghuwanshi <raghuwanshi.prerna@gmail.com>
2020-10-26 09:23:13 +0530

    [Prerna]REFACTORED: Overrided equals method

--- src/main/java/com/bl/InvoiceService/InvoiceSummary.java
+++ src/main/java/com/bl/InvoiceService/InvoiceSummary.java
@@ -1,9 +1,26 @@
 package com.bl.InvoiceService;
 
+import java.util.Objects;
+
 public class InvoiceSummary {
     private int totalRides;
     private double totalFare;
     private double averageFarePerRide;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        InvoiceSummary that = (InvoiceSummary) o;
+        return totalRides == that.totalRides &&
+                Double.compare(that.totalFare, totalFare) == 0 &&
+                Double.compare(that.averageFarePerRide, averageFarePerRide) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(totalRides, totalFare, averageFarePerRide);
+    }
 
     public InvoiceSummary(int totalRides, double totalFare) {
         this.totalFare = totalFare;
